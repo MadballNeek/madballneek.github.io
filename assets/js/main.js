7106ec32805c0c19c9eada2c15ca50604dcf1f09
Nick DiMucci <nickdimucci@mindshaftgames.com>
2015-11-18 12:54:11 -0500

    Disabling Milan artwork

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,10 +39,10 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-                            'images/bg01.jpg': 'center',
-                            'images/bg000.gif': 'center',
-                            'images/bg00.jpg': 'center',
+                            //'images/bg00.jpg': 'center',
                             'images/bg09.png': 'center',
+                            //'images/bg01.jpg': 'center',
+                            'images/bg000.gif': 'center',
                             'images/bg05.png': 'center',
                             'images/bg02.png': 'center',
                             'images/bg001.gif': 'center',
